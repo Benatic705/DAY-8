2fefe438a3bbe11f8c34af737d98ab44626cbced
Benatic705 <kit28.24bcs021@gmail.com>
2026-06-11 12:34:57 +0530

    Delete OneDrive/Documents/day 3 directory

--- OneDrive/Documents/day 3/encapsulation.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, title):
-        self.__title = title
-
-    def get_title(self):
-        return self.__title
-
-book = Book("Python Basics")
-print(book.get_title())
